5e32af9567187a2098c96f499493b0dd5bbf7e29
alexKillaZ <alexiskillaz02@gmail.com>
2025-09-15 14:14:32 -0600

    Agregando proyecto Spring Boot completo

---
+++ src/main/java/com/aatechsolutions/savorycloud/SavorycloudApplication.java
@@ -0,0 +1,13 @@
+package com.aatechsolutions.savorycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavorycloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavorycloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aatechsolutions/savorycloud/SavorycloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aatechsolutions.savorycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavorycloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
