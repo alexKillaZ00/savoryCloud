322c5ce7c51c875eaf4266f8a77cc874bffb98af
alexKillaZ <alexiskillaz02@gmail.com>
2025-09-15 14:48:05 -0600

    Se agregó otro comentario en ServletInitializer.java

--- src/main/java/com/aatechsolutions/savorycloud/ServletInitializer.java
+++ src/main/java/com/aatechsolutions/savorycloud/ServletInitializer.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	//hola soy armando
+	//hola soy alexis
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SavorycloudApplication.class);
