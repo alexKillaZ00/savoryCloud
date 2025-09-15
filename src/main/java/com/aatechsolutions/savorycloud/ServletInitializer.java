2230b1579582672d04e7d2435a8d4f83ff30102f
ArmandoArias1304 <armandoarias1302@gmail.com>
2025-09-15 14:39:08 -0600

    Se realizó un comentario

--- src/main/java/com/aatechsolutions/savorycloud/ServletInitializer.java
+++ src/main/java/com/aatechsolutions/savorycloud/ServletInitializer.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	//hola soy armando
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SavorycloudApplication.class);
